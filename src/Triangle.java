3d8deb4b1df47c821b5e6505588a5b1a13e7e21c
MariemAbdi <mariem.abdi2000@gmail.com>
2023-10-01 21:30:08 +0100

    API & Timeout Added

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Triangle {
 	private float base;
 	private float hauteur;
@@ -26,10 +32,50 @@ public class Triangle {
 		this.hauteur = hauteur;
 	}
 	
-	//CALCULATING THE TRAINGLE'S SURFACE
+	//CALCULATING THE TRAINGLE'S SURFACE (SIMPLY)
 	public float surface (){
 		return ((base * hauteur)/2);
 	}
+	
+	//CALCULATING THE TRAINGLE'S SURFACE (USING A CERTAIN API)
+	/*public float surface() {
+        //MAKE AN HTTP GET REQUEST TO API
+        try {
+            //CREATING URL FOR THE API
+            String apiUrl = "https://rapidapi.com/abrahaelill-GRIsGuS-TSY/api/math7/surface";
+            String queryParameters = "?base=" + base + "&hauteur=" + hauteur;
+            URL url = new URL(apiUrl + queryParameters);
+
+            //MAKE A CONNECTION WITH API
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("X-RapidAPI-Key", "YOUR_API_KEY"); // Replace with your API key
+
+            //THE RESPONSE FROM API
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String responseLine;
+                StringBuilder responseContent = new StringBuilder();
+
+                while ((responseLine = reader.readLine()) != null) {
+                    responseContent.append(responseLine);
+                }
+                reader.close();
+
+                //PARSING THE STRING RESULT WE GET TO A FLOAT AND RETURNING IT
+                return Float.parseFloat(responseContent.toString());
+            } else {
+                //HANDLING THE API'S ERROR IF THERE'S ANY
+                System.err.println("API request failed with response code: " + responseCode);
+                return -1.0f; //RETURNING A NEGATIVE NUMBER
+            }
+        } catch (IOException e) {
+            //HANDLING ANY EXEPTIONS DURING HTTP REQUEST
+            e.printStackTrace();
+            return -1.0f;  //RETURNING A NEGATIVE NUMBER
+        }
+    }*/
 	
 	//CALCULATING THE TRIANGLE'S PERIMETER
 	public float perimetre (){
--- src/TriangleTest.java
+++ src/TriangleTest.java
@@ -2,12 +2,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 //TEST CLASS FOR THE CLASS "TRIANGLE"
 public class TriangleTest {
 
-	//TESTING THE SURFACE FUNCTION
-    @Test
+	//TESTING THE SURFACE FUNCTION (SIMPLE)
+	@Test(timeout = 100) //TIMEOUT IN MILLISECONDS (THE RESPONSE SHOULD BE IN LESS THAN 0.1 SECOND)
     public void testSurface() {
         Triangle triangle = new Triangle(20.0f, 5.0f);
         assertEquals(50.0f, triangle.surface(), 0.001f);
